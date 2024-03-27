bb04122537a23883aa01d4742847e8d60852d3a2
THAIS RODRIGUES ANDRADE <thais.randrade5@senacsp.edu.br>
2024-03-26 21:11:54 -0300

    ok

--- public/app.js
+++ public/app.js
@@ -5,11 +5,13 @@ fetch('http://localhost:8080/hotias')
         data.array.forEach(hotel => {
             const hotelDiv = document.createElement('div');
             hotelDiv.innerHTML = `
-                <h2>${hotel.nome}</h2>
-                <img src="${hotel.foto}" alt="${hotel.nome}">
-                <p><strong>Descrição:</strong> ${hotel.descricao}</p>
-                <p><strong>Local:</strong> ${hotel.local}</p>
-                <p><strong>Valor:</strong> ${hotel.valor}</p>
+                <img src="${hotel.foto}" alt="${hotel.nome}" style="width:100px">
+                <div class="container">
+                    <h2>${hotel.nome}</h2>
+                    <p><strong>Descrição:</strong> ${hotel.descricao}</p>
+                    <p><strong>Local:</strong> ${hotel.local}</p>
+                    <p><strong>Valor:</strong> ${hotel.valor}</p>
+                </div>
             `;
             hoteisList.appendChild(hotelDiv);
         });
