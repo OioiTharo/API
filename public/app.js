128813df75640886c52bbb22488e542f22024a18
oioitharo <thais.randrade5@senacsp.edu.br>
2024-03-30 20:24:36 -0300

    ok

--- public/app.js
+++ public/app.js
@@ -2,10 +2,10 @@ fetch('http://localhost:8080/hoteis')
     .then(response => response.json())
     .then(data => {
         const hoteisList = document.getElementById('hoteis-list');
-        data.array.forEach(hotel => {
+        data.forEach(hotel => {
             const hotelDiv = document.createElement('div');
             hotelDiv.innerHTML = `
-                <img src="${hotel.foto}" alt="${hotel.nome}" style="width:100px">
+                <img src="${hotel.foto}" alt="${hotel.nome}" style="width:500px">
                 <div class="container">
                     <h2>${hotel.nome}</h2>
                     <p><strong>Descrição:</strong> ${hotel.descricao}</p>
@@ -16,4 +16,4 @@ fetch('http://localhost:8080/hoteis')
             hoteisList.appendChild(hotelDiv);
         });
     })
-    .catch(eror => console.error('Erro ao carregar os hoteis', error));
+    .catch(error => console.error('Erro ao carregar os hotéis', error));
